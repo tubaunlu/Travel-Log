ae15ea958f309931a6def965cea4f1417494a401
tubaunlu <tuba.unlu.011@gmail.com>
2025-08-09 12:54:38 +0300

    update

--- Travel_Log/app/test/widget_test.dart
+++ Travel_Log/app/test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:travel_log/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
